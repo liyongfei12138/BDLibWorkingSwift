dca6f78094b26a14cd371a403601d97cc8b6392a
李永飞 <miap972712779@qq.com>
2019-08-24 08:32:21 +0800

    1

--- BDLibWorkingSwift/Classes/BNMyViewController.swift
+++ BDLibWorkingSwift/Classes/BNMyViewController.swift
@@ -9,29 +9,29 @@
 import UIKit
 
 class BNMyViewController: UIViewController ,UIWebViewDelegate{
-
-
-    var urlString = String()
-    var progressView = BNProgressView(frame: CGRect(x: 0, y: kDeviceStatusHeight, width: kDeviceWidth, height: 4))
     
-    let myView = UIWebView(frame: CGRect(x: 0, y: 0, width: kDeviceWidth, height: kDeviceHeight ))
+    
+    var urlString = String()
+    var progressView = BNProgressView(frame: CGRect(x: 0, y: 0, width: kDeviceWidth, height: 4))
+    
+    let myView = UIWebView(frame: CGRect(x: 0, y: kDeviceStatusHeight, width: kDeviceWidth, height: kDeviceHeight - kDeviceStatusHeight))
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.navigationController?.navigationBar.setBackgroundImage(UIImage.from(color: UIColor.clear), for: UIBarMetrics.default)
-//        self.navigationController?.navigationBar.setsh
+        //        self.navigationController?.navigationBar.setsh
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.view.backgroundColor = UIColor.white
-
-       self.myView.backgroundColor = UIColor.white
+        
+        self.myView.backgroundColor = UIColor.white
         self.myView.delegate = self
-       self.view.addSubview(self.myView)
+        self.view.addSubview(self.myView)
         
         let url = URL(string: self.urlString)
         let request = URLRequest.init(url: url!, cachePolicy: URLRequest.CachePolicy(rawValue: 1)!, timeoutInterval: 30)
@@ -41,23 +41,23 @@ class BNMyViewController: UIViewController ,UIWebViewDelegate{
         self.myView.addSubview(self.progressView)
     }
     func webViewDidStartLoad(_ webView: UIWebView) {
-      
         
-     
-       self.progressView.beginLoad()
+        
+        
+        self.progressView.beginLoad()
     }
-
+    
     func webViewDidFinishLoad(_ webView: UIWebView) {
- 
-         self.progressView.finishLoad()
+        
+        self.progressView.finishLoad()
     }
     func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
         self.progressView.hideView()
     }
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
         
-         self.progressView.hideView()
-      
+        self.progressView.hideView()
+        
         return true
     }
 }
